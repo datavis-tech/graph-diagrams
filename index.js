4102766ef93e7bf62028719bbab0ce87809262ce
Curran <curran.kelleher@gmail.com>
2016-05-18 18:16:26 +0530

    Sketch API

--- index.js
+++ index.js
@@ -3,9 +3,18 @@ var app = express();
 
 app.use(express.static("public"));
 
-//app.get("/", function (req, res) {
-//  res.send("Hello World!");
-//});
+// Writes the metadata for the graph with the given name.
+app.post("/write/:graphName", function (req, res) {
+
+  // Write the data to public/graphs/{{graphName}}-meta.json
+  // * pan
+  // * zoom
+  // * fixedNodes: id ->
+  //   * x: int
+  //   * y: int
+
+  res.send("Success!");
+});
 
 app.listen(3000, function () {
   console.log("Example app listening on port 3000!");
